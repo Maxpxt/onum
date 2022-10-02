b1996f3b0a13845fdd6f2489e1d7c8672fe4e950
Maxwell <maxwellpxt@gmail.com>
2022-10-02 16:44:32 +0200

    Initial commit

---
+++ python/onum/__init__.py
@@ -0,0 +1,6 @@
+from .onum import *
+
+
+__doc__ = onum.__doc__
+if hasattr(onum, "__all__"):
+    __all__ = onum.__all__
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+use pyo3::prelude::*;
+
+#[pymodule]
+fn onum(_py: Python, m: &PyModule) -> PyResult<()> {
+    Ok(())
+}
