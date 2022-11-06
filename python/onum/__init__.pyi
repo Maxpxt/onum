cde223dcc9ad2d70285b44fe5060ae9299de4541
Maxwell <maxwellpxt@gmail.com>
2022-11-06 14:37:12 +0100

    Add a `.pyi`

---
+++ python/onum/__init__.pyi
@@ -0,0 +1,16 @@
+import numpy as np
+
+abconj: np.ufunc
+"""abconj(x1, x2, /, out=None, *, where=True, casting='same_kind', order='K', dtype=None, subok=True[, signature, extobj])
+
+``a`` times the complex conjugate of ``b``"""
+
+abs2: np.ufunc
+"""abs2(x, /, out=None, *, where=True, casting='same_kind', order='K', dtype=None, subok=True[, signature, extobj])
+
+Absolute value squared"""
+
+cis: np.ufunc
+"""cis(x, /, out=None, *, where=True, casting='same_kind', order='K', dtype=None, subok=True[, signature, extobj])
+
+Cosine plus i sine"""
